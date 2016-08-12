75971318eba2765e468d06059a450bf3aefde5a2
INOSTUDIO\d.belik <diasia.94@gmail.com>
2016-08-12 17:52:01 +0300

    видео fullpage.js раздел services

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+        scrollBar: true
+    });
+    $('.hero-video').on('canplaythrough', function() {
+        this.play();
+    });
+    $('#hero-change-slide').click( function(){
+        $.fn.fullpage.moveSectionDown();
+    })
+    $(window).scroll( function(){
+        $('.hideme').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},1000);
+
+            }
+
+        });
+    });
 });
